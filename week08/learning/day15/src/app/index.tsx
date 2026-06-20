132b687e79c591d0476bbfd83636c30951e29488
amie-dev <learn.amie01@gmail.com>
2026-06-20 11:22:19 +0530

    day 15 init

---
+++ week08/learning/day15/src/app/index.tsx
@@ -0,0 +1,27 @@
+import { Text, View, StyleSheet } from "react-native";
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
